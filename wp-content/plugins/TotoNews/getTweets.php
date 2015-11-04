f2c16a646509478e7186d0a3780a7abb27c040b5
jjkong15 <jjkong15@gmail.com>
2015-11-03 23:46:43 -0800

    sample getTweets final

--- wp-content/plugins/TotoNews/getTweets.php
+++ wp-content/plugins/TotoNews/getTweets.php
@@ -8,17 +8,25 @@ header('Content-type: application/xml');
 
 //get four variables: q (= keyword), lancode (=language code: ISO 639-1 alpha-2), latitude, longitude
 $theKeyword=urldecode($_REQUEST['q']);
+
+//return empty content if the keyword is empty (do this at very beginning to avoid unnecessary works if the keyword is empty )
+if(empty($theKeyword)){
+    echo '<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><item></item></channel></rss>'; 
+    return;
+}  
+
 $lancode=urldecode($_REQUEST['lancode']);
 $latitude=urldecode($_REQUEST['latitude']);
 $longitude=urldecode($_REQUEST['longitude']);
 
 //specify the location range for the tweets using latitude and longitude: within 10000 miles if both latitude and longitude are not empty
 $locationSt="";
-if(!empty($latitude) && !empty($longitude)){ $locationSt=$latitude.",".$longitude.",10000mi";}
+if(!empty($latitude) && !empty($longitude)){ 
+    $locationSt=$latitude.",".$longitude.",10000mi";
+}
 
 $st='<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><title>'.htmlspecialchars($theKeyword, ENT_XML1).'</title>';
 
-if($theKeyword==""){$st .= '<item></item></channel></rss>'; echo $st; return;}  //return empty content if the keyword is empty
 
 require_once 'Lib/twitteroauth-master/twitteroauth/twitteroauth.php';
 
@@ -26,6 +34,7 @@ require_once 'Lib/twitteroauth-master/twitteroauth/twitteroauth.php';
 function search(array $query)
 {
     //construct TwitterOAuth object. The following keys are fake keys for the sample purpose, you may get them by registering an application at https://dev.twitter.com/apps
+    //
     $consumer_key="1VswxxxxxxxxxxxxxxxxxxxxRil";
     $consumer_secret="C3WMV0xyISP8xxxxxxxxxxxxxxxxxlgcGw";
     $oauth_token="384927756-m32xxxxxxxxxxxxxxxxxxxxxxxxVuV";
@@ -35,10 +44,6 @@ function search(array $query)
     return $toa->get('search/tweets', $query);
 }
 
-    //"lang" => $lancode,
-    //"geocode" => $latitude.",".$longitude.",10000mi",
-
-//http://techiella.com/twitter-search-using-the-twitter-api-php/
 $query = array(
   "q" => $theKeyword,
   "count" => 80,
@@ -48,7 +53,8 @@ $query = array(
 );
 $results = search($query);
 
-if(count($results->statuses)<=3) {
+//if not many tweets (less than 3) return, look for global tweets (not specify geocode) 
+if(count($results->statuses)<=3) { //if you only get less than 3 tweets, then 
     $query = array(
         "q" => $theKeyword,
         "count" => 80,
@@ -57,6 +63,7 @@ if(count($results->statuses)<=3) {
     );
     $results = search($query);
 }
+//if still no tweets return, try to get the tweets without language restriction
 if(count($results->statuses)==0) {
     $query = array(
         "q" => $theKeyword,
@@ -66,41 +73,31 @@ if(count($results->statuses)==0) {
     $results = search($query);
 }
 
-//htmlspecialchars($contentSt, ENT_XML1)
-$contentSt="";
+//if still no tweets return, return empty content
+if(count($results->statuses)==0) {
+    echo '<?xml version="1.0" encoding="utf-8"?><rss version="2.0"><channel><item></item></channel></rss>'; 
+    return;
+}  
+
+//return all tweets
 foreach ($results->statuses as $result) {
     $userName=$result->user->screen_name;
     $idSt=$result->id;
     $contentSt = '<div><span>@<a style="color:#0084B4;" href="http://twitter.com/';
     $contentSt .= $userName.'" rel="external" target="_blank">';
     $contentSt .= $userName.'</a>:&nbsp;</span>'; 
-    //$contentSt .= '<span>' . $locationSt.$theKeyword.$lancode . '</span></div>';
     $contentSt .= '<span>' . $result->text . '</span></div>';
     $contentSt .= '<div style="font-size: 10px;text-align: center;color:#0084B4;">';
     $thelink='https://twitter.com/intent/tweet?in_reply_to='.$idSt;
     $theTitle='reply';
     $contentSt .= '<a href="' . $thelink . '" target="_BLANK">' . $theTitle . '</a>&nbsp;&nbsp;';
-    //$contentSt .= '<a href="https://twitter.com/intent/tweet?in_reply_to='.$idSt.'" target="_BLANK">reply</a>&nbsp;&nbsp;';
     $contentSt .= '<a href="https://twitter.com/intent/retweet?tweet_id='.$idSt.'" target="_blank">retweet</a>&nbsp;&nbsp;';
     $contentSt .= '<a href="https://twitter.com/intent/favorite?tweet_id='.$idSt.'" target="_blank">favorite</a>';
     $contentSt .= '</div>';
-    //$contentSt .=  "<span style='color:#0084B4;'>time: " . $result->created_at . ":&nbsp;</span><span class='author_name'>by " . $result->user->name . "</span><br />";
-    
-    $contentSt .=  "<br />";//time-ago
+    $contentSt .=  "<br />";
     $st .= '<item>';
     $st .=  '<description>'. htmlspecialchars($contentSt, ENT_XML1).'</description>';
-    $st .= '</item>';
-    
+    $st .= '</item>';    
 }
 $st .= '</channel></rss>';
 echo $st;
-//foreach($string as $items)
-//{
-//    echo "Time and Date of Tweet: ".$items['created_at']."<br />";
-//    echo "Tweet: ". $items['text']."<br />";
-//    echo "Tweeted by: ". $items['user']['name']."<br />";
-//    echo "Screen name: ". $items['user']['screen_name']."<br />";
-//    echo "Followers: ". $items['user']['followers_count']."<br />";
-//    echo "Friends: ". $items['user']['friends_count']."<br />";
-//    echo "Listed: ". $items['user']['listed_count']."<br /><hr />";
-//}
